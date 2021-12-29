eda474c5dce8fa9aae7355ba17732e5049c62a80
Joe Simpson <joe@radicalappdev.com>
2021-12-29 07:38:14 -0500

    Basic item card layout

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1,6 +1,5 @@
 import { Engine, Scene, ArcRotateCamera, Vector3, MeshBuilder, Mesh, StandardMaterial, Color3, HemisphericLight } from "@babylonjs/core";
-import { AdvancedDynamicTexture, StackPanel, TextBlock } from "@babylonjs/gui";
-
+import { AdvancedDynamicTexture, StackPanel, TextBlock, Image, Button } from "@babylonjs/gui";
 import { brand } from "@/helpers/brand";
 
 const createScene = (canvas) => {
@@ -9,10 +8,11 @@ const createScene = (canvas) => {
   const scene = new Scene(engine);
   createSceneEnvironment(scene);
   createSceneCamera(canvas);
-
   createSceneTitle();
   createLogo();
   createBlocks();
+
+  createItemCard();
 
   // Add a ground plane to the scene. Used for WebXR teleportation
   const ground = MeshBuilder.CreateGround("ground", { height: 50, width: 60, subdivisions: 4 });
@@ -32,6 +32,72 @@ const createScene = (canvas) => {
   engine.runRenderLoop(() => {
     scene.render();
   });
+};
+
+const createItemCard = () => {
+  const card = MeshBuilder.CreateBox("detail-card", { height: 3.2, width: 2, depth: 0.2 });
+  card.position = new Vector3(0, 2, 4);
+
+  const plane = MeshBuilder.CreatePlane("plane", { height: 3, width: 2 });
+  plane.position.z = -0.11;
+  plane.position.y = 0.1;
+  plane.parent = card;
+
+  const advancedTexture = AdvancedDynamicTexture.CreateForMesh(plane);
+
+  const panel = new StackPanel();
+  panel.verticalAlignment = 0;
+  // panel.height = "400px";
+  advancedTexture.addControl(panel);
+
+  const image = new Image("image", "https://extendedcollection.com/wp-content/uploads/2021/05/ec_logo_02.jpg");
+  image.height = "600px";
+  image.paddingTop = 40;
+  image.paddingLeft = 40;
+  image.paddingRight = 40;
+  panel.addControl(image);
+
+  const title = new TextBlock("title");
+  title.text = "Extended Collection";
+  title.color = "black";
+  title.fontSize = 48;
+  title.height = "100px";
+  title.textHorizontalAlignment = 0;
+  title.textVerticalAlignment = 0;
+  title.paddingTop = 40;
+  title.paddingLeft = 40;
+  title.paddingRight = 40;
+  panel.addControl(title);
+
+  const description = new TextBlock("description");
+  description.fontFamily = "Tahoma, sans-serif";
+  description.text =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  description.textWrapping = true;
+  description.color = "black";
+  description.fontSize = 24;
+  description.height = "660px";
+  description.textHorizontalAlignment = 0;
+  description.textVerticalAlignment = 0;
+  description.paddingTop = 20;
+  description.paddingLeft = 40;
+  description.paddingRight = 40;
+  panel.addControl(description);
+
+  const button1 = Button.CreateSimpleButton("but1", "Toggle Favorite");
+  button1.width = 1;
+  button1.height = "100px";
+  button1.color = "white";
+  button1.background = brand.pink;
+  button1.fontSize = 50;
+  button1.paddingBottom = 20;
+  button1.paddingLeft = 40;
+  button1.paddingRight = 40;
+  button1.onPointerUpObservable.add(function () {
+    console.log("button1 clicked");
+  });
+  button1.verticalAlignment = 1;
+  advancedTexture.addControl(button1);
 };
 
 const createSceneCamera = (canvas) => {
